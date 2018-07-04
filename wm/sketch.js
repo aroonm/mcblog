61132731472b9c9c6eea410c15ae9b26d1471ebc
xzhang395 <xuetingzhang313@gmail.com>
2018-07-04 15:13:52 -0400

    Fixed all step-ups in Walmart page
    Fixed all step-ups in Walmart page

--- wm/sketch.js
+++ wm/sketch.js
@@ -1,17 +1,13 @@
 var bg;
 function setup() {
-  // uncomment if for circle object of interest stepup
-  // bg = loadImage("hm/img/donuts.png");
+
   var cnv = createCanvas(300, 222);
   cnv.parent("draw-canvas");
 
-  // comment if for circle object of interest stepup
-  background(238, 238, 238);
 }
 
 function draw() {
-  // uncomment if for circle object of interest stepup
-  // background(bg);
+
 }
 
 function mouseDragged() {
